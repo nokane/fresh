a905ea3319d206e0caf55c40a2367babbb725a2a
Niall O'Kane <okanenm@gmail.com>
2018-07-24 13:41:20 -0600

    Log path

--- runner/watcher.go
+++ runner/watcher.go
@@ -39,6 +39,7 @@ func watchFolder(path string) {
 func watch() {
 	root := root()
 	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		watcherLog("Checking this path: %s", path)
 		if info.IsDir() && !isTmpDir(path) {
 			if len(path) > 1 && strings.HasPrefix(filepath.Base(path), ".") {
 				return filepath.SkipDir
